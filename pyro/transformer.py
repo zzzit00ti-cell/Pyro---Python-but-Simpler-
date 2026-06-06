1baea8bf083ee71c0e3ddb5f7b23751ec406ab4c
zzzit00ti-cell <zzzit00ti@gmail.com>
2026-06-06 15:46:22 -0500

    Refactor transformer.py and fix syntax issues
    Refactor PyTransformer class methods and fix syntax errors.

--- pyro/transformer.py
+++ pyro/transformer.py
@@ -1,8 +1,5 @@
 from .ast_nodes import *
 
-classd/pyro/transformer.py
-from .ast_nodes import *
-
 class PyTransformer:
     def __init__(self):
         self.indent = 0
@@ -14,18 +11,7 @@ class PyTransformer:
         if isinstance(node, Program):
             return "\n".join(self.transform(stmt) for stmt in node.statements)
         elif isinstance(node, FuncDef):
-            params PyTransformer:
-    def __init__(self):
-        self.indent = 0
-
-    def indent_str(self):
-        return "    " * self.indent
-
-    def transform(self, node):
-        if isinstance(node, Program):
-            return "\n".join(self.transform(stmt) for stmt in node.statements)
-        elif isinstance(node, FuncDef):
-            params = ", ".join(node.params = ", ".join(node.params)
+            params = ", ".join(node.params)
             self.indent += 1
             body = "\n".join(self.transform(stmt) for stmt in node.body)
             self.indent -= 1
@@ -36,40 +22,14 @@ class PyTransformer:
             for stmt in node.body:
                 if isinstance(stmt, Constructor):
                     params = stmt.params
-                    if not)
-            self.indent += 1
-            body = "\n".join(self.transform(stmt) for stmt in node.body)
-            self.indent -= 1
-            return f"def {node.name}({params}):\n{body}"
-        elif isinstance(node, ClassDef):
-            self.indent += 1
-            members = []
-            for stmt in node.body:
-                if isinstance(stmt, Constructor):
-                    params = stmt.params
-                    if not params or params[0] != params or params[0] != 'self':
+                    if not params or params[0] != 'self':
                         params.insert(0, 'self')
                     self.indent += 1
                     body = "\n".join(self.transform(s) for s in stmt.body)
                     self.indent -= 1
                     members.append(f"def __init__({', '.join(params)}):\n{body}")
                 elif isinstance(stmt, FuncDef):
-                    if 'self':
-                        params.insert(0, 'self')
-                    self.indent += 1
-                    body = "\n".join(self.transform(s) for s in stmt.body)
-                    self.indent -= 1
-                    members.append(f"def __init__({', '.join(params)}):\n{body}")
-                elif isinstance(stmt, FuncDef):
-                    if not stmt.params or stmt.params[0] !=  not stmt.params or stmt.params[0] != 'self':
-                        stmt.params.insert(0, 'self')
-                    members.append(self.transform(stmt))
-                else:
-                    members.append(self.transform(stmt))
-            self.indent -= 1
-            indent = self.indent_str()
-            body = "\n".join(f"{indent}    {m}" for m in members) if members else f"{indent}    pass"
-            return f"class {node.name'self':
+                    if not stmt.params or stmt.params[0] != 'self':
                         stmt.params.insert(0, 'self')
                     members.append(self.transform(stmt))
                 else:
@@ -78,17 +38,7 @@ class PyTransformer:
             indent = self.indent_str()
             body = "\n".join(f"{indent}    {m}" for m in members) if members else f"{indent}    pass"
             return f"class {node.name}:\n{body}"
-        elif isinstance(node, Constructor}:\n{body}"
         elif isinstance(node, Constructor):
-            params = node.params
-            if not params or params[0] != 'self':
-                params.insert(0, 'self')
-            self.indent += 1
-            body = "\n".join(self.transform(stmt) for stmt in node.body)
-            self.indent -= 1
-            return f"def __init__({', '.join(params)}):\n{body}"
-        elif isinstance(node, IfStmt):
-            cond =):
             params = node.params
             if not params or params[0] != 'self':
                 params.insert(0, 'self')
@@ -99,8 +49,6 @@ class PyTransformer:
         elif isinstance(node, IfStmt):
             cond = self.transform(node.cond)
             self.indent += 1
-            then self.transform(node.cond)
-            self.indent += 1
             then = "\n".join(self.transform(stmt) for stmt in node.then_body)
             self.indent -= 1
             result = f"if {cond}:\n{then}"
@@ -108,17 +56,7 @@ class PyTransformer:
                 self.indent += 1
                 else_ = "\n".join(self.transform(stmt) for stmt in node.else_body)
                 self.indent -= 1
-                result += f"\n = "\n".join(self.transform(stmt) for stmt in node.then_body)
-            self.indent -= 1
-            result = f"if {cond}:\n{then}"
-            if node.else_body:
-                self.indent += 1
-                else_ = "\n".join(self.transform(stmt) for stmt in node.else_body)
-                self.indent -= 1
                 result += f"\nelse:\n{else_}"
-            return result
-        elif isinstance(node, ForStmt):
-            varelse:\n{else_}"
             return result
         elif isinstance(node, ForStmt):
             var = node.var
@@ -131,41 +69,6 @@ class PyTransformer:
             return f"for {var} in {iterable}:\n{body}"
         elif isinstance(node, WhileStmt):
             cond = self.transform(node.cond)
-            self.indent += 1
-            body = "\n".join(self.transform(stmt) for stmt in node.body)
-            self.indent -= 1
-            return f"while {cond}:\n{body}"
-        elif isinstance(node, Assign):
-            value = self.transform(node.value)
-            return f"{node.target} = {value}"
-        elif isinstance(node, MemberAssign):
-            obj = self.transform(node.obj)
-            member = node.member
-            value = self.transform(node.value = node.var
-            iterable = self.transform(node.iterable)
-            if isinstance(node.iterable, Range):
-                iterable = f"range({self.transform(node.iterable.start)}, {self.transform(node.iterable.end)} + 1)"
-            self.indent += 1
-            body = "\n".join(self.transform(stmt) for stmt in node.body)
-            self.indent -= 1
-            return f"for {var} in {iterable}:\n{body}"
-        elif isinstance(node, WhileStmt):
-            cond = self.transform)
-            return f"{obj}.{member} = {value}"
-        elif isinstance(node, ExprStmt):
-            expr = self.transform(node.expr)
-            return f"{expr}"
-        elif isinstance(node, Return):
-            if node.value:
-                val = self.transform(node.value)
-                return f"return {val}"
-            return "return"
-        elif isinstance(node, BinOp):
-            left = self.transform(node.left)
-            right = self.transform(node.right)
-            return f"({left} {node.op} {right})"
-        elif isinstance(node, Number):
-            return str(node.value)(node.cond)
             self.indent += 1
             body = "\n".join(self.transform(stmt) for stmt in node.body)
             self.indent -= 1
@@ -188,7 +91,9 @@ class PyTransformer:
         elif isinstance(node, BinOp):
             left = self.transform(node.left)
             right = self.transform(node.right)
-            return
+            return f"({left} {node.op} {right})"
+        elif isinstance(node, Number):
+            return str(node.value)
         elif isinstance(node, String):
             return repr(node.value)
         elif isinstance(node, Var):
@@ -204,17 +109,4 @@ class PyTransformer:
             args = ", ".join(self.transform(a) for a in node.args)
             return f"{func}({args})"
         else:
-            raise TypeError(f"Unknown AST node: {type f"({left} {node.op} {right})"
-        elif isinstance(node, Number):
-            return str(node.value)
-        elif isinstance(node, String):
-            return repr(node.value)
-        elif isinstance(node, Var):
-            return node.name
-        elif isinstance(node, This):
-            return "self"
-        elif isinstance(node, Range):
-            start = self.transform(node.start)
-            end = self.transform(node.end)
-            return f"range({start}, {end}+1)"
-        elif isinstance(node, Call(node)}")
+            raise TypeError(f"Unknown AST node: {type(node)}")
