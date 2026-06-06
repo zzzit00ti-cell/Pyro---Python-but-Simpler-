22a4507789b0cb70f6ef531015acbbce3c1215df
zzzit00ti-cell <zzzit00ti@gmail.com>
2026-06-06 15:15:16 -0500

    Bump version from 0.1.0 to 0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="pyro-lang",
-    version="0.1.0",
+    version="0.1.3",
     author="Pyro Contributors",
     description="A friendlier Python that compiles to Python",
     long_description=long_description,
@@ -18,8 +18,6 @@ setup(
     },
     packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
